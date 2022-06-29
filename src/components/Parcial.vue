a462f510f11eb280ff1cd31643ab9daed4c3f1f7
Martin Corizzo <macorizzo@gmail.com>
2022-06-28 21:29:46 -0300

    first commit

---
+++ src/components/Parcial.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <section class="src-components-parcial">
+    <h1>Recuperatorio Corizzo Martin PNT2-21F-YA</h1>
+
+    <div>
+      <input type="text" v-model="texto" placeholder="Ingresa texto"/>
+    </div>
+    <hr>
+      <p><u>Resultado</u></p>
+      
+      <div v-if="texto.length">
+        <p> {{ cuentaCaracteres }}</p>
+        <p>1 - {{ texto | Codificado}} (codificado)</p>
+        <p>2 - {{ texto | Mayuscula }} (mayuscula)</p>
+        <p>3 - {{ texto | Minuscula }} (minuscula)</p>
+        <!-- <p>4 - {{ texto | Intercalado1 }} (intercalado1)</p> -->
+        <!-- <p>4 - {{ texto }} </p>
+        <p> cantidad caracteres: {{ cuentaCaracteres }}</p> -->
+      </div>
+      
+      
+      
+      <br>
+
+      <p>Respuestas: 1:C 2:B 3:C 4:A 5:B .... </p>
+     
+
+      <!-- <hr>
+      <p><u>formState</u></p>
+      <pre>{{ tamanioString }}</pre> -->
+
+  </section>
+ 
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-parcial',
+    props: [],
+    mounted () {},
+    data () {
+      return {
+        texto: [],
+      }
+    },
+    methods: {
+      
+      
+
+    },
+    computed: {
+      cuentaCaracteres() {
+        let caracteres = 0;
+        for (let i = 0; i < this.texto.length(); i++) {
+          caracteres++;
+        }
+        return caracteres;
+      },
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-parcial {
+
+  }
+</style>
---
+++ src/globalFilters.js
@@ -0,0 +1,30 @@
+
+import Vue from "vue";
+
+Vue.filter("Mayuscula", function (value) {
+  return value.toUpperCase();
+});
+Vue.filter("Minuscula", function (value) {
+    return value.toLowerCase();
+});
+
+Vue.filter("Codificado", function (value) {
+    let nuevoTexto = value.toLowerCase();
+    nuevoTexto = nuevoTexto.replace('a', 'u').replace('e', 'o');
+    /* nuevoTexto = nuevoTexto.replace('e', 'o')
+    nuevoTexto = nuevoTexto.replace('e', 'o')
+    nuevoTexto = nuevoTexto.replace('e', 'o') */
+    return nuevoTexto;
+}); 
+
+Vue.filter("Intercalado1", function (value) {
+    let cadena = value;
+        cadena.array.forEach(element => {
+            if (element.index() % 2 == 0) {
+                element.toLowerCase();
+            }
+        });
+
+    
+    return cadena;
+}); 
